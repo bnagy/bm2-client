7dad01ceca45e72082f99eda4c27ac51659a5fd9
Ben Nagy <github@ben.iagu.net>
2010-12-17 17:30:15 +0545

    fix requires

--- word_fuzzclient.rb
+++ word_fuzzclient.rb
@@ -16,6 +16,7 @@ require File.dirname(__FILE__) + '/../bm2-core/connector'
 require File.dirname(__FILE__) + '/conn_office'
 require File.dirname(__FILE__) + '/conn_cdb'
 require File.dirname(__FILE__) + '/word_delivery_agent'
+require 'fileutils'
 require 'rubygems'
 require 'trollop'
 
