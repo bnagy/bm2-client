8904f9c0df6868e4ca9c14354ec9ec6c692e8610
Ben Nagy <github@ben.iagu.net>
2010-12-24 12:10:30 +0545

    update .bat for new code paths

--- startfuzz.bat
+++ startfuzz.bat
@@ -7,12 +7,12 @@ rmdir /s /q c:\fuzzbot_code
 REM This ping is because the network goes offline for a while
 REM with kvm-qemu after power cycling a bunch of fuzzbots
 ping -n 90 192.168.122.1
-xcopy /d /y /s \\192.168.122.1\ramdisk\fuzzbot_code c:\fuzzbot_code\
+xcopy /d /y /s \\192.168.122.1\ramdisk\fuzzbot_code\bm2-client c:\fuzzbot_code\
 copy /y c:\fuzzbot_code\startfuzz.bat c:\AUTOEXEC.BAT
-c:\fuzzbot_code\compname /c BUGMINER-?8
+c:\compname /c BUGMINER-?8
 rmdir /s /q r:\fuzzclient
 mkdir r:\fuzzclient
 REM CHANGE THIS TO THE CORRECT PATH
-C:\WinDDK\Debuggers\gflags /p /enable WINWORD.EXE /full
-cd c:\fuzzbot_code\word_fuzzbot
+"C:\Program Files\Debugging Tools for Windows (x86)\gflags" /p /enable WINWORD.EXE /full
+cd c:\fuzzbot_code
 start cmd /k ruby word_fuzzclient.rb
